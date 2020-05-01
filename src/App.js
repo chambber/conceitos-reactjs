8c8cbfa5652d628775c31ca903a17131f41d7261
Rubens Pereira do Nascimento <iorubenspn@gmail.com>
2020-05-01 03:30:42 -0300

    feat: load repositories

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from "./services/api";
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    const loadRepositories = async () => {
+      const { data } = await api.get("/repositories");
+      setRepositories(data);
+    };
+
+    loadRepositories();
+  }, []);
+
   async function handleAddRepository() {
     // TODO
   }
@@ -14,8 +26,9 @@ function App() {
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {repositories.map(({ id, title }) => (
+          <li key={id}>
+            {title}
 
           <button onClick={() => handleRemoveRepository(1)}>
             Remover
